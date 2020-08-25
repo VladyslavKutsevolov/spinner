eea3a07cf27abd1af8fe87c63a1740e7aba82480
Vladyslav <kytsevolov@gmail.com>
2020-08-25 14:20:46 -0600

    refactored spinner

---
+++ spinner2.js
@@ -0,0 +1,8 @@
+let counter = 0;
+let spinner = ['\r|   ', '\r/   ', '\r-   ', '\r\\   ', '\r|   ', '\r/   '];
+for (let i = 0; i < spinner.length; i++) {
+  setTimeout(() => {
+    process.stdout.write(spinner[i]);
+  }, counter);
+  counter += 300;
+}
